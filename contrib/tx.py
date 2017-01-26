874df75c2c6870a2111e7887b0e9ddf52be01ea6
Dominik Heidler <dheidler@suse.de>
2017-01-26 13:51:34 +0100

    AMQP: Declare exchange passive and durable
    This will allow the rabbitmq user to use less privileges.
    The exchange needs to be created by the rabbitmq admin.

--- contrib/tx.py
+++ contrib/tx.py
@@ -6,7 +6,7 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='kazhua.suse.de'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='pubsub', type='topic')
+channel.exchange_declare(exchange='pubsub', type='topic', passive=True, durable=True)
 
 routing_key = sys.argv[1] if len(sys.argv) > 1 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
