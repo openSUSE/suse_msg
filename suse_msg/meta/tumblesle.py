1dd549f267c63dd04520769b4aa886d03baad41d
Oliver Kurz <okurz@suse.de>
2017-04-27 10:36:35 +0200

    tumblesle: Prevent too many similar messages

--- suse_msg/meta/tumblesle.py
+++ suse_msg/meta/tumblesle.py
@@ -1,10 +1,22 @@
+import logging
 import re
+from collections import deque
 from suse_msg.meta import BaseProcessor
 
 
-# limit is 512 including all characters, just split it somewhere
-MSG_LIMIT = 400
+logging.basicConfig(level=logging.INFO)
 
+
+# limit is 512 including all characters, just split it somewhere. 400 seems
+# to be a sensible value. Reduce it further to limit the line of messages
+# pumped out to users in IRC. Reduced *even more* to act as a workaround by
+# limiting the strings so much that there will probably be only one message
+# per build about regressions unless there is a status change
+MSG_LIMIT = 12
+
+# limit of how many times a truncated message has to show up the same before we repeat it
+SEEN_LIMIT = 4
+SEEN = deque(maxlen=SEEN_LIMIT)
 
 def truncate(s):
     if len(s) > MSG_LIMIT:
@@ -31,6 +43,12 @@ class TumbleSLEProcessor(BaseProcessor):
         # prevent irc.client.MessageTooLong
         msg = ', '.join('%s: %s' % (k, truncate(comma_entries(v))) for k, v in self.msg.items())
         s = "tumbleSLE %s: %s" % (result, msg)
+        if s in SEEN:
+            logging.info("Ignoring already seen message '%s'" % s)
+            return ''
+        else:
+            logging.info("not found s in self.seen. self.seen: %s" % SEEN)
+        SEEN.append(s)
         return s
 
     def colored_result(self, c):
