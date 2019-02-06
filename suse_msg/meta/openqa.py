eae7a6b467eabba775643dfc0cf72d0fa777009e
Dominik Heidler <dheidler@suse.de>
2019-02-06 16:00:57 +0100

    Fix None vs undefined

--- suse_msg/meta/openqa.py
+++ suse_msg/meta/openqa.py
@@ -20,7 +20,7 @@ class OpenQAProcessor(BaseProcessor):
                 s += " with result "
                 s += self.colored_job_result(c)
                 if self.msg.get('result') == 'failed':
-                    s += " (%s)" % self.msg.get('bugref', 'review pending')
+                    s += " (%s)" % (self.msg.get('bugref') or 'review pending')
             s += ": " + self.job_url()
         elif self.object == 'comment':
             if self.is_group_event():
